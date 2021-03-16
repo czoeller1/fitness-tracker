87bd9b2ff5ef508eb911a9bf9c1f07bf4e66c2ea
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-15 20:55:26 -0600

    populated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,34 @@
+const db = require("..models/");
+
 module.exports = (app) => {
-  app.get("/api/workouts", (req, res) => {});
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then((data) => res.json(data));
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Exercise.create(body)
+      .then(({ _id }) =>
+        db.Workout.findOneAndUpdate(
+          { _id: req.params.id },
+          { $push: { exercises: _id } },
+          { new: true }
+        )
+      )
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  app.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body).then((workout) => res.json(workout));
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .sort({ day: -1 })
+      .limit(7)
+      .then((workouts) => res.json(workouts));
+  });
 };
